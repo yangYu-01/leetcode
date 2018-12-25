bf4c86c44f8ce734d2de942e073982c112b0b460
yangYu <691554336@qq.com>
2018-12-25 13:36:26 +0800

    Create 004.Median of Two Sorted Arrays_online.py

---
+++ 004.Median of Two Sorted Arrays_online.py
@@ -0,0 +1,37 @@
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        def findKth(A, B, k):
+            if len(A) == 0:
+                return B[k-1]
+            if len(B) == 0:
+                return A[k-1]
+            if k == 1:
+                return min(A[0], B[0])
+            
+            if len(B) < k // 2  :
+                return findKth(A[k // 2:], B, k - k // 2)
+            # a = A[k // 2 - 1]
+            # b = B[k // 2 - 1]
+            elif (len(A) >= k//2 and A[k // 2 - 1] <= B[k // 2 - 1]):
+                return findKth(A[k // 2:], B, k - k // 2)
+            
+            elif len(A) < k // 2:
+                return findKth(A, B[k // 2:], k - k // 2)
+            # a = A[k // 2 - 1]
+            # b = B[k // 2 - 1]
+            elif (len(B) >= k//2 and A[k // 2 - 1] >= B[k // 2 - 1]):
+                return findKth(A, B[k // 2:], k - k // 2)
+            
+        n = len(nums1) + len(nums2)
+        if n % 2 == 1:
+            return findKth(nums1, nums2, n // 2 + 1)
+        else:
+            smaller = findKth(nums1, nums2, n // 2)
+            bigger = findKth(nums1, nums2, n // 2 + 1)
+            return (smaller + bigger) / 2.0
