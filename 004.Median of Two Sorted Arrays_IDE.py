7ce1bc6533feb14cacd014638ba4e78465b48941
yangYu <691554336@qq.com>
2018-12-27 14:41:54 +0800

    Update 004.Median of Two Sorted Arrays_IDE.py

--- 004.Median of Two Sorted Arrays_IDE.py
+++ 004.Median of Two Sorted Arrays_IDE.py
@@ -10,18 +10,15 @@ def findKth(A, B, k):
     if k == 1:
         return min(A[0], B[0])
 
-    #<原文>
+
     # a = A[k // 2 - 1] if len(A) >= k // 2 else None
     # b = B[k // 2 - 1] if len(B) >= k // 2 else None
 
     # if b is None or (a is not None and a < b):
     #     return findKth(A[k // 2:], B, k - k // 2)
     # return findKth(A, B[k // 2:], k - k // 2)  # 这里要注意：因为 k/2 不一定 等于 (k - k/2)
-    # </原文>
+ 
 
-
-    #<改写>
-    # 把原文1个判断（高手写法），改写成4个判断，容易理解
 
     # 第1个判断
     if len(B) < k // 2:
@@ -38,7 +35,7 @@ def findKth(A, B, k):
     # b = B[k // 2 - 1]
     elif (len(B) >= k//2 and A[k // 2 - 1] >= B[k // 2 - 1]):
         return findKth(A, B[k // 2:], k - k // 2)
-    #</改写>
+  
 
 def findMedianSortedArrays(nums1, nums2):
     """
@@ -60,22 +57,4 @@ if __name__ == '__main__':
     output = findMedianSortedArrays(nums1, nums2)
     print(output)
 
-# 错误集锦
-#error_1.
-    # 第1个判断，elif (len(A) >= k//2 and A[k // 2 - 1] <【=】 B[k // 2 - 1]):
-    # 少=，4个if/elif都不进去，nonetype报错。
-    # 第4个判断同理。
-    # nums1 = [1,1]
-    # nums2 = [1,1]
-#error_2.
-    # 第3个判断elif：return findKth(A, B[k // 2:], k - k // 2)，
-    # 第4个判断elif：return findKth(A[k // 2:], B, k - k // 2)，所做操作不一样。第4个判断没改。
-    # nums1 = [1,2]
-    # nums2 = [-1,3]
-#error_3.
-    # nums1 = [2]
-    # nums2 = [1]
-    # 第1个判断，if len(B) <【=】 k // 2:，自己改写，多了一个=，这种情况bigger跳进第一个判断，使bigger=1，结果为1。答案1.5。
-    # 第3个判断，elif len(A) <【=】 k // 2:，多了一个=
-    # nums1 = [2]
-    # nums2 = [1]
+
